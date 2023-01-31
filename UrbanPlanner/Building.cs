df64803f81c5de0a1e0ae3a4a368a01f5b2a5d8a
alvinwendt <alvinwendt@gmail.com>
2023-01-30 19:57:05 -0600

    Add project files.

---
+++ UrbanPlanner/Building.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace UrbanPlanner
+{
+    internal class Building
+    {
+       
+//        ### Private Fields
+
+//- `_designer` of type `string`. It will hold your name.
+//- `_dateConstructed` of type `DateTime`. This will hold the exact time the building was created.
+//- `_address` of type string.
+//- `_owner` of type string. This will store the name of the person who owns the building.
+
+//### Public Properties
+
+//- `Stories` typed as an integer.
+//- `Width` typed as a double.
+//- `Depth` typed as a double.
+//- `Volume` should be read-only and should return *width* depth * (3 * # of stories)*. Each story is 3 meters high.
+
+//    ### Constructor
+
+//Define a constructor method that accepts a single string argument - `address` - so that you can send as a parameter when you create each instance with `new Building("800 8th Street");`.
+
+//- The constructor’s logic should set the `_address` field’s value to the argument value
+
+//### Public Methods
+
+//- Define a `Construct()` method.The method’s logic should set the `_dateConstructed` field’s value to `DateTime.Now`.
+//- Define a `Purchase()` method.The method should accept a single string argument of the name of the person purchasing a building.The method should take that string and assign it to the private `_owner` field.
+
+//Neither of these methods will return a value.
+
+    }
+}
---
+++ UrbanPlanner/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System;
+
+namespace UrbanPlanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //### Build Your City
+
+            //            1.Create several buildings in the `Main()` method of `Program.cs`.
+    
+            //    ```csharp
+            //    Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            //    ```
+    
+            //2.Give each building a width, height, and number of stories.
+            //3.Construct each building.
+            //4.Have business people in your city purchase each of your buildings.
+
+            //After all of the buildings have been purchased, display the following information to the console for each building.
+
+
+        }
+    }
+}
